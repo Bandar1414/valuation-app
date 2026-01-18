2c5ab02af12695ebb7347a6f1aa59b6b10d3f560
Bandar1414 <bandar14141489@gmail.com>
2026-01-18 22:40:21 +0300

    Fix run config for production

--- run.py
+++ run.py
@@ -1,15 +1,15 @@
 from app import create_app
+
 import matplotlib
-matplotlib.rcParams['font.family'] = 'sans-serif'
-matplotlib.rcParams['font.sans-serif'] = ['Arial']  # أو أي خط متاح
+matplotlib.use("Agg")  # مهم للسيرفرات بدون واجهة رسومية
+matplotlib.rcParams["font.family"] = "sans-serif"
+matplotlib.rcParams["font.sans-serif"] = ["Arial"]
+
 from app.visitor_tracker import get_visitor_count
+
 print("📈 عدد الزوار الآن:", get_visitor_count())
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
-
-if __name__ == "__main__":
-    app = create_app()
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
